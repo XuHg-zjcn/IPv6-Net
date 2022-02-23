3bcb6f1bcda97a6f9abb77236635d77766a63fb1
徐瑞骏 <1687701765@qq.com>
2022-02-23 15:10:12 +0800

    写入hosts文件

--- main.py
+++ main.py
@@ -2,21 +2,28 @@
 import random
 import time
 
-from db import htab
-from ip46 import ClientUDP
 from threading import Thread
 from queue import Queue
+from python_hosts import Hosts, HostsEntry
 
+from db import htab
+from ip46 import ClientUDP
+
+
+hosts = Hosts(path='hosts_test')
 queue = Queue()
+
 
 class Host(Thread):
     def __init__(self, table, did):
         self.table = table
         self.did = did
-        self.period, self.ipv4, self.ipv6 = table.get_conds_onlyone({'id':did}, fields=('test_period', 'ipv4', 'ipv6'))
+        self.hname, self.period, self.ipv4, self.ipv6 = \
+            table.get_conds_onlyone({'id':did}, fields=('name', 'test_period', 'ipv4', 'ipv6'))
         Thread.__init__(self)
 
     def update(self):
+        resp = None
         for i in range(10):
             try:
                 resp = ClientUDP(self.ipv4)
@@ -32,10 +39,10 @@ class Host(Thread):
             if len(words) == 2 and words[0] == 'inet6':
                 ipv6 = words[1]
         if ipv6 != self.ipv6:
-            queue.put((self.did, ipv6))
+            queue.put((self.hname, self.did, ipv6))
             self.ipv6 = ipv6
         else:
-            queue.put((self.did,))
+            queue.put((self.hname, self.did,))
 
     def run(self):
         self.update()
@@ -43,6 +50,7 @@ class Host(Thread):
         while True:
             self.update()
             time.sleep(self.period)
+
 
 if __name__ == '__main__':
     hlst = []
@@ -52,7 +60,15 @@ if __name__ == '__main__':
         h.start()
     while True:
         ele = queue.get()
-        if len(ele) == 1:
-            htab.inc_time(*ele)
+        if len(ele) == 2:
+            htab.inc_time(ele[1])
+            hname = ele[0]
+            print(f'{hname} : keep')
         else:
-            htab.update_ipv6(*ele)
+            htab.update_ipv6(*ele[1:])
+            hname, did, ipv6 = ele
+            hosts.remove_all_matching('ipv6', hname)
+            target = HostsEntry(entry_type='ipv6', address=ipv6, names=[hname])
+            hosts.add([target])
+            hosts.write()
+            print(f'{hname} : {ipv6}')
