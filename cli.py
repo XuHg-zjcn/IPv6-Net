798fc8d7288286e4361362dd4c6792f047eebf19
徐瑞骏 <1687701765@qq.com>
2022-03-20 00:37:00 +0800

    添加GNU GPLv3许可证

--- cli.py
+++ cli.py
@@ -1,11 +1,32 @@
 #!/usr/bin/python3
+########################################################################
+'''CLI交互操作数据库'''
+# Copyright (C) 2022  Xu Ruijun
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+########################################################################
 import datetime
 import sys
 from prettytable import PrettyTable
 from db import htab
 
 version_str = '''
-版本v0.1.0-2
+IPv6Net v0.1.0-3
+Copyright (C) 2022  Xu Ruijun
+许可证 GPLv3+：GNU 通用公共许可证第 3 版或更新版本。
+本程序是自由软件：您可以自由修改和重新发布它。
+在法律范围内没有任何担保。
 '''
 
 def ts2str(x):
