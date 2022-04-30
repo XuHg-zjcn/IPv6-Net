1692b13d1f7a1612792c470a8a8acc8c7493a63b
徐瑞骏 <1687701765@qq.com>
2022-04-30 22:48:26 +0800

    修改spec.md和相应enum

--- protol.py
+++ protol.py
@@ -2,17 +2,17 @@
 from enum import Enum
 
 
-class Commd(Enum):  #
-    GTN = 0x00      # None
-    PTN = 0x01      # str: name
-    GTA = 0x02      # None
-    PTA = 0x03      # >I:version, 16B:ipv6
-    GTS = 0x04
-    PTS = 0x05
-    GOL = 0x06
-    POL = 0x07
-    GON = 0x08
-    PON = 0x09
-    GOA = 0x0A
-    POA = 0x0B
-    STAT = 0x0C      #
+class Commd(Enum): # Params
+    GN = 0x00      #
+    PN = 0x01      # Ver, Len, Name, Sign
+    GA = 0x02      #
+    PA = 0x03      # Ver, IPv6, Sign
+    GK = 0x04      #
+    PK = 0x05      # Pub
+    GI = 0x06      #
+    PI = 0x07      # Ver, Len, Info, Sign
+    GL = 0x08      #
+    PL = 0x09      #
+    TG = 0x0A      # Pub
+    NF = 0x0B      #
+    STAT = 0x0C    #
